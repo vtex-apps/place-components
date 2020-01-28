5253e04274756ff475152ead5c580a349645f1fc
Maxi <maximiliano.redionda@vtex.com.br>
2020-01-28 18:18:59 -0300

    Deletes comments, and fixes translation

--- react/Example.tsx
+++ react/Example.tsx
@@ -1,35 +1,12 @@
-import React /*, { useState }*/ from 'react'
-//import PlaceDetails from './PlaceDetails'
+import React from 'react'
 import AddressForm from './AddressForm'
-//import { Dropdown } from 'vtex.styleguide'
 import { sampleAddress as address } from './mocks/CountryDescriptions'
 import { AddressContextProvider } from 'vtex.address-context/AddressContext'
-//import { Display } from './typings/countryRulesTypes.d'
 import { LocaleSwitcher } from 'vtex.locale-switcher'
 
 const Example: StorefrontFunctionComponent<{}> = () => {
-  /*const [display, setDisplay] = useState<keyof Display>('extended')
-
-  const onDisplayDropdownChange = (_: Event, newDisplay: keyof Display) => {
-    setDisplay(newDisplay)
-  }*/
-
   return (
     <div>
-      <h1 className="tc">Place Components</h1>
-      {/*<h2>Place Details:</h2>
-      <Dropdown
-        label="Display"
-        options={[
-          { value: 'minimal', label: 'minimal' },
-          { value: 'compact', label: 'compact' },
-          { value: 'extended', label: 'extended' },
-        ]}
-        value={display}
-        onChange={onDisplayDropdownChange}
-        className="w4"
-      />
-      <PlaceDetails display={display} />*/}
       <h2>AddressForm:</h2>
       <AddressForm />
     </div>
