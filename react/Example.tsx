45f46bf8bfbd0c092a9260a2162f8c759f5dc13e
Maxi <maximiliano.redionda@vtex.com.br>
2020-02-04 16:56:10 -0300

    First LocationSelect version [WIP]

--- react/Example.tsx
+++ react/Example.tsx
@@ -1,25 +1,48 @@
 import React from 'react'
 import AddressForm from './AddressForm'
 import LocationSelect from './LocationSelect'
-import { sampleAddress as address } from './mocks/CountryDescriptions'
+import {
+  sampleAddress1 as address1,
+  sampleAddress2 as address2,
+} from './mocks/CountryDescriptions'
 import { AddressContextProvider } from 'vtex.address-context/AddressContext'
 import { LocaleSwitcher } from 'vtex.locale-switcher'
 
-const Example: StorefrontFunctionComponent<{}> = () => {
+const Example1: StorefrontFunctionComponent<{}> = () => {
   return (
     <div>
       <h2>AddressForm:</h2>
       <AddressForm />
+    </div>
+  )
+}
+
+const Example2: StorefrontFunctionComponent<{}> = () => {
+  return (
+    <div>
+      <h2>LocationSelect</h2>
       <LocationSelect />
     </div>
   )
 }
 
 const ExampleWrapper = () => (
-  <AddressContextProvider address={address} countries={['BRA', 'KOR']}>
-    <LocaleSwitcher />
-    <Example />
-  </AddressContextProvider>
+  <div>
+    <AddressContextProvider
+      address={address1}
+      countries={['BRA', 'KOR', 'ARG']}
+    >
+      <LocaleSwitcher />
+      <Example1 />
+    </AddressContextProvider>
+    <AddressContextProvider
+      address={address2}
+      countries={['BRA', 'KOR', 'ARG']}
+    >
+      <LocaleSwitcher />
+      <Example2 />
+    </AddressContextProvider>
+  </div>
 )
 
 export default ExampleWrapper
--- react/LocationSelect.tsx
+++ react/LocationSelect.tsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import rules from './countries/rules'
+import React, { useState } from 'react'
 import { useAddressContext } from 'vtex.address-context/AddressContext'
+import rules from './countries/rules'
+import { Dropdown } from 'vtex.styleguide'
 
-const LocationSelect: StorefrontFunctionComponent<{}> = ({}) => {
+/*
+const LocationSelect: StorefrontFunctionComponent<{}> = () => {
   const { address } = useAddressContext()
   const countryRules = rules[address.country]
+  const countryData = countryRules?.locationSelect?.countryData
+  const fields = countryRules?.locationSelect?.fields
+  const [completed , setCompleted] = useState([])
 
-  return <div>{countryRules ? <div>Hola</div> : <div>Chau</div>}</div>
+  return fields.map((
+    { name, label } _: any,
+    index: number , fields
+  ) => {
+    const getDropdownOptions = () => {
+      return Object.keys(
+        completed.reduce((obj, curr) => obj[curr], countryData)
+      )
+    }
+
+    const getDropdownProps = () => {
+      return {
+        disabled: index > completed.length,
+        options: index == completed.length ? getDropdownOptions() : [],
+      }
+    }
+
+    return <Dropdown {...getDropdownProps()} key={index} />
+  })
+}*/
+
+const LocationSelect: StorefrontFunctionComponent<{}> = () => {
+  const { address } = useAddressContext()
+  const countryRules = rules[address.country]
+  if (!countryRules.locationSelect) {
+    return (
+      <div>
+        The LocationSelect component is not applicable to this country :'(
+      </div>
+    )
+  }
+  const { countryData, fields } = countryRules.locationSelect
+  const [completed, setCompleted] = useState([])
+
+  console.log(countryData)
+  console.log(fields)
+  console.log(setCompleted)
+
+  return (
+    <div>
+      {fields.map((_: any, index: number) => {
+        const getDropdownOptions = () => {
+          return Object.keys(countryData)
+        }
+
+        const getDropdownProps = () => {
+          return {
+            disabled: index > completed.length,
+            options: index == completed.length ? getDropdownOptions() : [],
+          }
+        }
+
+        return <Dropdown {...getDropdownProps()} key={index} />
+      })}
+    </div>
+  )
 }
 
 export default LocationSelect
