1a8289eca054f3639404b39f6c20e3219e7bf3b9
Maxi <maximiliano.redionda@vtex.com.br>
2020-02-13 16:35:58 -0300

    Adds color option to custom SVG icons, and renders them conditionally on component state.

--- react/DeviceCoordinates.tsx
+++ react/DeviceCoordinates.tsx
@@ -3,18 +3,24 @@ import { useAddressContext } from 'vtex.address-context/AddressContext'
 import { ButtonPlain } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 import { LocationRequestIcon } from './images/LocationRequestIcon'
-import { LocationConceivedIcon } from './images/LocationRequestIcon'
+import { LocationConceivedIcon } from './images/LocationConceivedIcon'
 import { Spinner } from 'vtex.styleguide'
 
 enum State {
   OFF,
   ON,
   LOADING,
+  DENIED,
+}
+
+const colors = {
+  blue: '#134cd8',
+  gray: '#979899',
 }
 
 const DeviceCoordinates: StorefrontFunctionComponent<{}> = () => {
   const { address, setAddress, countries } = useAddressContext()
-  const [state, setState] = useState<State>(State.OFF)
+  const [state, setState] = useState<State>(State.DENIED)
 
   console.log(address, setAddress, countries, setState)
 
@@ -23,22 +29,34 @@ const DeviceCoordinates: StorefrontFunctionComponent<{}> = () => {
 
     switch (state) {
       case State.OFF:
-        icon = <LocationRequestIcon />
+        icon = <LocationRequestIcon color={colors.blue} />
         break
       case State.ON:
-        icon = <LocationConceivedIcon />
+        icon = <LocationConceivedIcon color={colors.blue} />
         break
       case State.LOADING:
-        icon = <Spinner />
+        icon = <Spinner size={20} />
+        break
+      case State.DENIED:
+        icon = <LocationRequestIcon color={colors.gray} />
         break
     }
 
     return icon
   }
 
+  const onButtonClick = () => {
+    alert('hey')
+  }
+
+  const buttonProps = {
+    disabled: state === State.DENIED,
+    onClick: onButtonClick,
+  }
+
   return (
     <div>
-      <ButtonPlain>
+      <ButtonPlain {...buttonProps}>
         {renderIcon()}
         <FormattedMessage id="place-components.label.useCurrentLocation" />
       </ButtonPlain>
