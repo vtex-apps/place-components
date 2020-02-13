7338e54d636a0bc0d3949928a0bfb3bd9d1cbfc6
Maxi <maximiliano.redionda@vtex.com.br>
2020-02-13 18:00:29 -0300

    Adds tooltip when permission was denied

--- react/DeviceCoordinates.tsx
+++ react/DeviceCoordinates.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import { useAddressContext } from 'vtex.address-context/AddressContext'
-import { ButtonPlain } from 'vtex.styleguide'
+import { ButtonPlain, Spinner, Tooltip } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 import { LocationRequestIcon } from './images/LocationRequestIcon'
 import { LocationConceivedIcon } from './images/LocationConceivedIcon'
-import { Spinner } from 'vtex.styleguide'
 
 enum State {
   OFF,
@@ -23,27 +22,6 @@ const DeviceCoordinates: StorefrontFunctionComponent<{}> = () => {
   const [state, setState] = useState<State>(State.OFF)
 
   console.log(address, setAddress, countries, setState)
-
-  const renderIcon = () => {
-    let icon = null
-
-    switch (state) {
-      case State.OFF:
-        icon = <LocationRequestIcon color={colors.blue} />
-        break
-      case State.ON:
-        icon = <LocationConceivedIcon color={colors.blue} />
-        break
-      case State.LOADING:
-        icon = <Spinner size={20} />
-        break
-      case State.DENIED:
-        icon = <LocationRequestIcon color={colors.gray} />
-        break
-    }
-
-    return icon
-  }
 
   const onGetCurrentPositionSuccess = ({ coords }: Position) => {
     console.log(coords.latitude)
@@ -69,19 +47,53 @@ const DeviceCoordinates: StorefrontFunctionComponent<{}> = () => {
         enableHighAccuracy: true,
       }
     )
+    setState(
+      State.LOADING
+    ) /* No guarantee that this will run before onGetCurrentPositionSuccess setState! */
   }
 
-  const buttonProps = {
-    disabled: state === State.DENIED,
-    onClick: onButtonClick,
+  const renderIcon = () => {
+    let icon = null
+
+    switch (state) {
+      case State.OFF:
+        icon = <LocationRequestIcon color={colors.blue} />
+        break
+      case State.ON:
+        icon = <LocationConceivedIcon color={colors.blue} />
+        break
+      case State.LOADING:
+        icon = <Spinner size={16} />
+        break
+      case State.DENIED:
+        icon = <LocationRequestIcon color={colors.gray} />
+        break
+    }
+
+    return icon
   }
 
-  return (
-    <div>
+  const renderButton = () => {
+    const buttonProps = {
+      disabled: state === State.DENIED,
+      onClick: onButtonClick,
+    }
+
+    return (
       <ButtonPlain {...buttonProps}>
         {renderIcon()}
         <FormattedMessage id="place-components.label.useCurrentLocation" />
       </ButtonPlain>
+    )
+  }
+
+  return (
+    <div>
+      {state === State.DENIED ? (
+        <Tooltip label="Permission not granted">{renderButton()}</Tooltip>
+      ) : (
+        renderButton()
+      )}
     </div>
   )
 }
