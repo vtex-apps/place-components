e8aea555c9b37fe8043ce0ad60a4e0baf180802e
Maxi <maximiliano.redionda@vtex.com.br>
2020-02-17 13:15:21 -0300

    Completes tests for LocationCountry

--- react/__tests__/LocationCountry.test.tsx
+++ react/__tests__/LocationCountry.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@vtex/test-tools/react'
+import { render, fireEvent } from '@vtex/test-tools/react'
 import { AddressContextProvider } from 'vtex.address-context/AddressContext'
 import { Address } from 'vtex.checkout-graphql'
 import LocationCountry from '../LocationCountry'
@@ -21,13 +21,38 @@ describe('Location Country', () => {
     const { queryAllByText } = renderComponent(address, countryListWithBrazil)
 
     expect(queryAllByText('Argentina')).toHaveLength(1)
-    expect(queryAllByText('Brazil')).toHaveLength(
-      2
-    ) /* Showed as option and as selected value */
+    expect(queryAllByText('Brazil')).toHaveLength(2)
     expect(queryAllByText('Korea')).toHaveLength(1)
   })
 
   it('should throw error if country is not in countryList', () => {
     expect(() => renderComponent(address, countryListWithoutBrazil)).toThrow()
+  })
+
+  it('should change country when clicking on another option', () => {
+    const { getByLabelText, queryAllByText } = renderComponent(
+      address,
+      countryListWithBrazil
+    )
+
+    fireEvent.change(getByLabelText(/Country/i), { target: { value: 'ARG' } })
+
+    expect(queryAllByText('Argentina')).toHaveLength(2)
+    expect(queryAllByText('Brazil')).toHaveLength(1)
+    expect(queryAllByText('Korea')).toHaveLength(1)
+  })
+
+  it('should be able to go back to first country after a change', () => {
+    const { getByLabelText, queryAllByText } = renderComponent(
+      address,
+      countryListWithBrazil
+    )
+
+    fireEvent.change(getByLabelText(/Country/i), { target: { value: 'ARG' } })
+    fireEvent.change(getByLabelText(/Country/i), { target: { value: 'BRA' } })
+
+    expect(queryAllByText('Argentina')).toHaveLength(1)
+    expect(queryAllByText('Brazil')).toHaveLength(2)
+    expect(queryAllByText('Korea')).toHaveLength(1)
   })
 })
