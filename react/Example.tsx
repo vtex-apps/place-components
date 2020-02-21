7ea6bc565a612f5ed088746e38fce9ebbb1cd626
Maxi <maximiliano.redigonda@vtex.com.br>
2020-02-21 14:37:57 -0300

    Adds styling to DeviceCoordinates

--- react/Example.tsx
+++ react/Example.tsx
@@ -80,7 +80,7 @@ const TestExample: StorefrontFunctionComponent = () => {
 }
 
 const ExampleWrapper = () => (
-  <div>
+  <div className="w-30-ns pa4">
     <AddressContextProvider address={address1} countries={countryList}>
       <LocaleSwitcher />
       <Example1 />
