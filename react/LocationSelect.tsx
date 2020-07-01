998726aa3eaf25d5effc5718e369f6bf9b812c06
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-07-01 09:33:47 -0300

    fix type error

--- react/LocationSelect.tsx
+++ react/LocationSelect.tsx
@@ -68,7 +68,11 @@ const LocationSelect: React.FC = () => {
 
     for (let i = 0; i < fields.length; ++i) {
       const field = fields[i]
-      const fieldValue = address[field.name!]!
+      const fieldValue = address[field.name!]
+
+      if (typeof fieldValue !== 'string') {
+        continue
+      }
 
       locationSelects.push(
         <div className="mb5" key={i}>
