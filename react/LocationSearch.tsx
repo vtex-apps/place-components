3afab923f8e122ab936520eea174ba53c8c99d4d
Wesley Rocha <wesleysr1997@gmail.com>
2020-07-03 19:47:00 -0300

    Add clear button
    Notes:
    - it isn't a button...
    - what tabIndex does, exactly?
    - not vertically centralized :/

--- react/LocationSearch.tsx
+++ react/LocationSearch.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import { Input, IconSearch } from 'vtex.styleguide'
+import { Input, IconSearch, IconClear } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 
 const LocationSearch: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('')
+
+  const onClear = () => setInputValue('')
 
   return (
     <div className="w-100">
@@ -12,6 +14,19 @@ const LocationSearch: React.FC = () => {
           <FormattedMessage id="place-components.label.autocompleteAddress" />
         }
         prefix={<IconSearch color="#134CD8" />}
+        suffix={
+          inputValue.length && (
+            <span
+              role="button"
+              tabIndex={0}
+              className="pointer"
+              onClick={onClear}
+              onKeyPress={onClear}
+            >
+              <IconClear color="#CACBCC" />
+            </span>
+          )
+        }
         value={inputValue}
         onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
           setInputValue(e.target.value)
