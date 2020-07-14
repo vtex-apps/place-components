8971cf1190ec2ad493cfb44fedac30b468300a4b
Wesley Rocha <wesleysr1997@gmail.com>
2020-07-14 17:46:01 -0300

    Wrap input component inside combobox

--- react/LocationSearch.tsx
+++ react/LocationSearch.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Input, IconSearch, IconClear } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
+import { Combobox, ComboboxInput } from '@reach/combobox'
 
 import styles from './LocationSearch.css'
 import { addresses as mockedAddresses } from './addresses'
@@ -17,35 +18,39 @@ const LocationSearch: React.FC = () => {
 
   return (
     <div className={`${styles.locationSearch} w-100`}>
-      <Input
-        testId="location-search-input"
-        label={
-          <FormattedMessage id="place-components.label.autocompleteAddress" />
-        }
-        prefix={
-          <div className="c-action-primary flex justify-center items-center">
-            <IconSearch />
-          </div>
-        }
-        suffix={
-          inputValue.length && (
-            <span
-              role="button"
-              tabIndex={-1}
-              className="pointer c-muted-3 flex justify-center items-center outline-0"
-              onClick={() => setInputValue('')}
-              onKeyPress={() => {}}
-            >
-              <IconClear />
-            </span>
-          )
-        }
-        value={inputValue}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setInputValue(event.target.value)
-        }
-        onKeyDown={handleKeyDown}
-      />
+      <Combobox>
+        <ComboboxInput
+          as={Input}
+          testId="location-search-input"
+          label={
+            <FormattedMessage id="place-components.label.autocompleteAddress" />
+          }
+          prefix={
+            <div className="c-action-primary flex justify-center items-center">
+              <IconSearch />
+            </div>
+          }
+          suffix={
+            inputValue.length && (
+              <span
+                data-testid="location-search-clear"
+                role="button"
+                tabIndex={-1}
+                className="pointer c-muted-3 flex justify-center items-center outline-0"
+                onClick={() => setInputValue('')}
+                onKeyPress={() => {}}
+              >
+                <IconClear />
+              </span>
+            )
+          }
+          value={inputValue}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setInputValue(event.target.value)
+          }
+          onKeyDown={handleKeyDown}
+        />
+      </Combobox>
     </div>
   )
 }
