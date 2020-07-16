7615cfe98d731716aaeb258f2c4941684f97059e
Wesley Rocha <wesleysr1997@gmail.com>
2020-07-16 18:49:32 -0300

    Add drop-down logic of Location Search autocomplete
    Notes:
    - Added temporary mocked addresses from mockaroo
    - Using a Reach UI Combobox component as basis
    
    Issues:
    - While navigating on options with the keyboard, the input text will "blink" when clicking the clear input button

--- react/LocationSearch.tsx
+++ react/LocationSearch.tsx
@@ -1,50 +1,96 @@
-import React, { useState } from 'react'
-import { Input, IconSearch, IconClear } from 'vtex.styleguide'
+import React, { useState, useMemo } from 'react'
 import { FormattedMessage } from 'react-intl'
+import { Input, IconSearch, IconClear } from 'vtex.styleguide'
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxPopover,
+  ComboboxList,
+} from '@reach/combobox'
+import '@reach/combobox/styles.css'
 
+import { addresses as mockedAddresses } from './addresses'
 import styles from './LocationSearch.css'
 
-const LocationSearch: React.FC = () => {
-  const [inputValue, setInputValue] = useState<string>('')
+const MAX_DROPDOWN_ADDRESSES = 6
+
+// This function will be replaced in the future, after integrating the
+// component with GraphQL queries.
+const getAddresses = (searchTerm: string) => {
+  return mockedAddresses
+    .filter((address: string) =>
+      address.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+    )
+    .slice(0, MAX_DROPDOWN_ADDRESSES)
+}
+
+interface LocationSearchProps {
+  onSelectAddress?: (selectedAddress: string) => void
+}
+
+const LocationSearch: React.FC<LocationSearchProps> = ({ onSelectAddress }) => {
+  const [searchTerm, setSearchTerm] = useState<string>('')
+  const addresses = useMemo(() => getAddresses(searchTerm), [searchTerm])
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key !== 'Escape') {
       return
     }
-    setInputValue('')
+    setSearchTerm('')
+  }
+
+  const handleAddressSelection = (selectedAddress: string) => {
+    setSearchTerm(selectedAddress)
+    onSelectAddress?.(selectedAddress)
   }
 
   return (
     <div className={`${styles.locationSearch} w-100`}>
-      <Input
-        testId="location-search-input"
-        label={
-          <FormattedMessage id="place-components.label.autocompleteAddress" />
-        }
-        prefix={
-          <div className="c-action-primary flex justify-center items-center">
-            <IconSearch />
-          </div>
-        }
-        suffix={
-          inputValue.length && (
-            <span
-              role="button"
-              tabIndex={-1}
-              className="pointer c-muted-3 flex justify-center items-center outline-0"
-              onClick={() => setInputValue('')}
-              onKeyPress={() => {}}
-            >
-              <IconClear />
-            </span>
-          )
-        }
-        value={inputValue}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setInputValue(event.target.value)
-        }
-        onKeyDown={handleKeyDown}
-      />
+      <Combobox onSelect={handleAddressSelection}>
+        <ComboboxInput
+          as={Input}
+          testId="location-search-input"
+          label={
+            <FormattedMessage id="place-components.label.autocompleteAddress" />
+          }
+          prefix={
+            <div className="c-action-primary flex justify-center items-center">
+              <IconSearch />
+            </div>
+          }
+          suffix={
+            searchTerm.trim().length && (
+              <span
+                data-testid="location-search-clear"
+                role="button"
+                tabIndex={-1}
+                className="pointer c-muted-3 flex justify-center items-center outline-0"
+                onClick={() => setSearchTerm('')}
+                onKeyPress={() => {}}
+              >
+                <IconClear />
+              </span>
+            )
+          }
+          value={searchTerm}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchTerm(event.target.value)
+          }
+          onKeyDown={handleKeyDown}
+        />
+        <ComboboxPopover>
+          {addresses.length > 0 ? (
+            <ComboboxList>
+              {addresses.map((address, index) => (
+                <ComboboxOption value={address} key={index} />
+              ))}
+            </ComboboxList>
+          ) : (
+            <FormattedMessage id="place-components.label.autocompleteAddressFail" />
+          )}
+        </ComboboxPopover>
+      </Combobox>
     </div>
   )
 }
