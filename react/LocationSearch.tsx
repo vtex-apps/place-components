7719b9fd6e72959f92fa8aa95c851efb51a3ff6e
Wesley Rocha <wesleysr1997@gmail.com>
2020-08-21 20:28:59 -0400

    Add debounce, loading state and data fetching

--- react/LocationSearch.tsx
+++ react/LocationSearch.tsx
@@ -1,8 +1,19 @@
-import React, { useState, useMemo, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Input, IconSearch, IconClear, IconWarning } from 'vtex.styleguide'
+import {
+  Input,
+  IconSearch,
+  IconClear,
+  IconWarning,
+  Spinner,
+} from 'vtex.styleguide'
 import { positionMatchWidth } from '@reach/popover'
+import { useAddressContext } from 'vtex.address-context/AddressContext'
+import { useLazyQuery } from 'react-apollo'
+import { Address, AddressSuggestion } from 'vtex.places-graphql'
 
+import SUGGEST_ADDRESSES from './graphql/suggestAddresses.graphql'
+import GET_ADDRESS_BY_EXTERNAL_ID from './graphql/getAddressByExternalId.graphql'
 import {
   Combobox,
   ComboboxInput,
@@ -12,27 +23,32 @@ import {
 } from './components/Combobox'
 import PlaceIcon from './components/PlaceIcon'
 
-interface Interval {
-  offset: number
-  size: number
+const DEBOUNCE_DELAY_IN_MS = 500
+
+const useDebouncedValue = (value: string, delayInMs: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delayInMs)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delayInMs])
+
+  return debouncedValue
 }
 
-export interface Suggestion {
-  description: string
-  mainText: string
-  mainTextMatchInterval: Interval
-  secondaryText: string
-}
-
-const renderSuggestionText = (suggestion: Suggestion) => {
+const renderSuggestionText = (suggestion: AddressSuggestion) => {
   const { mainText } = suggestion
-  const { offset, size } = suggestion.mainTextMatchInterval
+  const { offset, length } = suggestion.mainTextMatchInterval
   return (
     <div className="truncate c-muted-2">
       <span className="c-on-base">
         {mainText.substr(0, offset)}
-        <span className="b">{mainText.substr(offset, size)}</span>
-        {mainText.substr(size + offset)}
+        <span className="b">{mainText.substr(offset, length)}</span>
+        {mainText.substr(length + offset)}
       </span>{' '}
       <span>{suggestion.secondaryText}</span>
     </div>
@@ -40,22 +56,68 @@ const renderSuggestionText = (suggestion: Suggestion) => {
 }
 
 interface LocationSearchProps {
-  getAddresses: (searchTerm: string) => Suggestion[]
-  onSelectAddress?: (selectedAddress: string) => void
+  onSelectAddress?: (address: Address) => void
   renderEngineLogo?: () => React.ReactNode
 }
 
 const LocationSearch: React.FC<LocationSearchProps> = ({
-  getAddresses,
   onSelectAddress,
   renderEngineLogo,
 }) => {
-  const [searchTerm, setSearchTerm] = useState<string>('')
-  const addresses = useMemo(() => getAddresses(searchTerm), [
-    getAddresses,
-    searchTerm,
-  ])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [suggestions, setSuggestions] = useState<AddressSuggestion[]>([])
   const inputWrapperRef = useRef<HTMLDivElement>(null)
+  const { setAddress } = useAddressContext()
+  const debouncedSearchTerm = useDebouncedValue(
+    searchTerm,
+    DEBOUNCE_DELAY_IN_MS
+  )
+  const [
+    executeSuggestAddresses,
+    {
+      data: suggestAddressesData,
+      error: suggestAddressesError,
+      loading: suggestAddressesLoading,
+    },
+  ] = useLazyQuery(SUGGEST_ADDRESSES)
+  const [
+    executeGetAddressByExternalId,
+    { data: getAddressByExternalIdData, error: getAddressByExternalIdError },
+  ] = useLazyQuery(GET_ADDRESS_BY_EXTERNAL_ID)
+
+  useEffect(() => {
+    if (debouncedSearchTerm.trim().length) {
+      executeSuggestAddresses({
+        variables: { searchTerm: debouncedSearchTerm },
+      })
+    } else {
+      setSuggestions([])
+    }
+  }, [debouncedSearchTerm, executeSuggestAddresses])
+
+  useEffect(() => {
+    if (suggestAddressesData) {
+      setSuggestions(suggestAddressesData.suggestAddresses)
+    }
+    if (suggestAddressesError) {
+      console.warn(suggestAddressesError.message)
+    }
+  }, [suggestAddressesData, suggestAddressesError, setSuggestions])
+
+  useEffect(() => {
+    if (getAddressByExternalIdData) {
+      setAddress(getAddressByExternalIdData.getAddressByExternalId)
+      onSelectAddress?.(getAddressByExternalIdData.getAddressByExternalId)
+    }
+    if (getAddressByExternalIdError) {
+      console.warn(getAddressByExternalIdError.message)
+    }
+  }, [
+    getAddressByExternalIdData,
+    getAddressByExternalIdError,
+    onSelectAddress,
+    setAddress,
+  ])
 
   const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key !== 'Escape') {
@@ -66,7 +128,10 @@ const LocationSearch: React.FC<LocationSearchProps> = ({
 
   const handleAddressSelection = (selectedAddress: string) => {
     setSearchTerm(selectedAddress)
-    onSelectAddress?.(selectedAddress)
+    const id = suggestions.find(
+      address => address.description === selectedAddress
+    )?.externalId
+    executeGetAddressByExternalId({ variables: { id } })
   }
 
   return (
@@ -116,23 +181,27 @@ const LocationSearch: React.FC<LocationSearchProps> = ({
               )
             }
           >
-            {addresses.length > 0 ? (
-              <>
-                <ComboboxList>
-                  {addresses.map((address, index) => (
-                    <ComboboxOption value={address.description} key={index}>
-                      <PlaceIcon className="flex flex-shrink-0 mr4 c-muted-1" />
-                      {renderSuggestionText(address)}
-                    </ComboboxOption>
-                  ))}
-                </ComboboxList>
-                {renderEngineLogo && (
-                  <div className="flex flex-row-reverse">
-                    <div className="mt3 mb1 mh5">{renderEngineLogo()}</div>
-                  </div>
-                )}
-              </>
-            ) : (
+            {suggestions.length > 0 ? (
+              <ComboboxList>
+                {suggestions.map((address, index) => (
+                  <ComboboxOption value={address.description} key={index}>
+                    <PlaceIcon className="flex flex-shrink-0 mr4 c-muted-1" />
+                    {renderSuggestionText(address)}
+                  </ComboboxOption>
+                ))}
+              </ComboboxList>
+            ) : null}
+            {suggestAddressesLoading ? (
+              <div className="flex flex-row justify-center items-center pa3">
+                <Spinner size={20} />
+              </div>
+            ) : null}
+            {suggestions.length > 0 && renderEngineLogo ? (
+              <div className="flex flex-row-reverse">
+                <div className="mt3 mb1 mh5">{renderEngineLogo()}</div>
+              </div>
+            ) : null}
+            {!suggestAddressesLoading && suggestions.length === 0 ? (
               <div className="flex items-center pv3 ph5">
                 <div className="flex flex-shrink-0 mr4 c-muted-3">
                   <IconWarning />
@@ -141,7 +210,7 @@ const LocationSearch: React.FC<LocationSearchProps> = ({
                   <FormattedMessage id="place-components.label.autocompleteAddressFail" />
                 </div>
               </div>
-            )}
+            ) : null}
           </ComboboxPopover>
         ) : null}
       </Combobox>
