2e47258dab81509b3ecfba5a46e72a30c783fe98
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-11-12 16:00:45 -0300

    Filter empty lines and fragments

--- react/PlaceDetails.tsx
+++ react/PlaceDetails.tsx
@@ -36,53 +36,57 @@ const PlaceDetails: React.FC<Props> = ({
 
   return (
     <div className="flex flex-column">
-      {displaySpec.map((line, displayIndex) => (
-        <div className="flex" key={displayIndex}>
-          {line.map((fragment, index) => {
-            if (
-              !address[fragment.name] ||
-              hiddenFields.includes(fragment.name)
-            ) {
-              return null
-            }
+      {displaySpec
+        .map(line => {
+          return line.filter(
+            fragment =>
+              address[fragment.name] && !hiddenFields.includes(fragment.name)
+          )
+        })
+        .filter(line => line.length > 0)
+        .map((line, displayIndex) => (
+          <div className="flex" key={displayIndex}>
+            {line.map((fragment, index) => {
+              const hasPreviousFragment =
+                index > 0 && address[line[index - 1].name]
+              const hasNextFragment =
+                index + 1 < line.length && address[line[index + 1].name]
+              const hasDifferentDelimiter = fragment.delimiterAfter !== '-'
+              const shouldShowDelimiter =
+                hasNextFragment ?? hasDifferentDelimiter
 
-            const hasPreviousFragment =
-              index > 0 && address[line[index - 1].name]
-            const hasNextFragment =
-              index + 1 < line.length && address[line[index + 1].name]
-            const hasDifferentDelimiter = fragment.delimiterAfter !== '-'
-            const shouldShowDelimiter = hasNextFragment ?? hasDifferentDelimiter
+              const valueMask =
+                fragment.name in countryRules.fields
+                  ? countryRules.fields[fragment.name]?.mask
+                  : undefined
 
-            const valueMask =
-              fragment.name in countryRules.fields
-                ? countryRules.fields[fragment.name]?.mask
-                : undefined
+              const addressValue = valueMask
+                ? msk(address[fragment.name] as string, valueMask)
+                : address[fragment.name]
 
-            const addressValue = valueMask
-              ? msk(address[fragment.name] as string, valueMask)
-              : address[fragment.name]
-
-            return (
-              <div className="dib" key={fragment.name}>
-                {fragment.delimiter && hasPreviousFragment && (
-                  <span>{fragment.delimiter}</span>
-                )}
-                <span className={fragment.name}>{addressValue}</span>
-                {fragment.delimiterAfter && shouldShowDelimiter && (
-                  <span>{fragment.delimiterAfter}</span>
-                )}
+              return (
+                <div className="dib" key={fragment.name}>
+                  {fragment.delimiter && hasPreviousFragment && (
+                    <span>{fragment.delimiter}</span>
+                  )}
+                  <span className={fragment.name}>{addressValue}</span>
+                  {fragment.delimiterAfter && shouldShowDelimiter && (
+                    <span>{fragment.delimiterAfter}</span>
+                  )}
+                </div>
+              )
+            })}
+            {onEdit && displayIndex === 0 && (
+              <div className="ml4 flex items-center">
+                <ButtonPlain onClick={onEdit}>
+                  <span className="ttl">
+                    {intl.formatMessage(messages.edit)}
+                  </span>
+                </ButtonPlain>
               </div>
-            )
-          })}
-          {onEdit && displayIndex === 0 && (
-            <div className="ml4 flex items-center">
-              <ButtonPlain onClick={onEdit}>
-                <span className="ttl">{intl.formatMessage(messages.edit)}</span>
-              </ButtonPlain>
-            </div>
-          )}
-        </div>
-      ))}
+            )}
+          </div>
+        ))}
     </div>
   )
 }
