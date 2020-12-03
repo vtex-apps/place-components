c096f2479ec2f821525985fc6cda95178fb060d5
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-03 12:20:35 -0400

    Organize and add fixtures

---
+++ react/__fixtures__/address.fixture.tsx
@@ -0,0 +1,65 @@
+import { Address } from 'vtex.places-graphql'
+
+export const emptyAddress: Address = {
+  street: '',
+  number: '',
+  complement: '',
+  postalCode: '',
+  city: '',
+  state: '',
+  addressId: '',
+  addressType: null,
+  country: '',
+  geoCoordinates: [],
+  neighborhood: '',
+  receiverName: null,
+  reference: null,
+}
+
+export const completeAddress: Address = {
+  street: 'Av. Belgrano',
+  number: '2248',
+  complement: 'B',
+  postalCode: '2000',
+  city: 'Rosario',
+  state: 'Santa Fé',
+  addressId: '',
+  addressType: null,
+  country: 'ARG',
+  geoCoordinates: [],
+  neighborhood: 'Las Delicias',
+  receiverName: null,
+  reference: null,
+}
+
+export const incompleteAddress: Address = {
+  street: 'Av. Belgrano',
+  number: '2248',
+  complement: '',
+  postalCode: '2000',
+  city: '',
+  state: '',
+  addressId: '',
+  addressType: null,
+  country: 'ARG',
+  geoCoordinates: [],
+  neighborhood: '',
+  receiverName: null,
+  reference: null,
+}
+
+export const sampleAddress: Address = {
+  street: 'Rua Afonso Camargo',
+  number: '805',
+  complement: '',
+  postalCode: '85070-200',
+  city: 'Guarapuava',
+  state: 'PR',
+  addressId: '',
+  addressType: null,
+  country: 'BRA',
+  geoCoordinates: [],
+  neighborhood: 'Santana',
+  receiverName: null,
+  reference: null,
+}
