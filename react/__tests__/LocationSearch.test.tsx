2be1f7c5d9fe227564fa777847e5c3b19965d575
Wesley Rocha <wesleysr1997@gmail.com>
2020-12-04 10:07:38 -0400

    Update tests

--- react/__tests__/LocationSearch.test.tsx
+++ react/__tests__/LocationSearch.test.tsx
@@ -1,18 +1,53 @@
 import React from 'react'
 import { render, screen, fireEvent } from '@vtex/test-tools/react'
+import { MockedProvider, MockedResponse } from '@apollo/react-testing'
+import { Address } from 'vtex.checkout-graphql'
+import { AddressRules } from 'vtex.address-context/types'
 
 import LocationSearch from '../LocationSearch'
-import { getFewAddresses } from '../__fixtures__/fixtures'
+import { AddressContextProvider } from '../__mocks__/vtex.address-context/AddressContext'
+import { googleLogo } from '../__fixtures__/graphql/providerLogo.fixture'
+import {
+  noSuggestions,
+  simpleSuggestions,
+} from '../__fixtures__/graphql/suggestAddresses.fixture'
+import { simpleSuggestedAddress } from '../__fixtures__/graphql/getAddressByExternalId.fixture'
 
 const UNTABABBLE = -1
+const LABEL_MATCHER = /Address \(street, number, square\.\.\.\)/i
+
+interface RenderComponentProps {
+  address?: Address | null
+  countries?: string[]
+  rules?: AddressRules
+  graphqlMocks?: MockedResponse[]
+}
 
 describe('Location Search', () => {
+  const renderComponent = ({
+    address = null,
+    countries = [],
+    rules = {},
+    graphqlMocks = [],
+  }: RenderComponentProps) => {
+    return render(
+      <MockedProvider addTypename={false} mocks={graphqlMocks}>
+        <AddressContextProvider
+          address={address}
+          countries={countries}
+          rules={rules}
+        >
+          <LocationSearch />
+        </AddressContextProvider>
+      </MockedProvider>
+    )
+  }
   it('should be able to select a suggested address when a search term is found', async () => {
-    render(<LocationSearch getAddresses={getFewAddresses} />)
+    renderComponent({
+      graphqlMocks: [googleLogo, simpleSuggestions, simpleSuggestedAddress],
+    })
 
-    const input = screen.getByLabelText(
-      /Address and house number/i
-    ) as HTMLInputElement
+    const input = screen.getByLabelText(LABEL_MATCHER)
     fireEvent.change(input, { target: { value: 'Praia de Botafogo' } })
 
     expect(
@@ -26,16 +61,18 @@ describe('Location Search', () => {
     expect(input).toHaveValue(
       'Praia de Botafogo, 200, Botafogo, Rio de Janeiro, RJ'
     )
+    // same as waiting loading to finish
+    expect(await screen.findByRole('button')).toBeInTheDocument()
+    fireEvent.blur(input)
   })
 
-  it('should be able to clear the input when something is typed', () => {
-    render(<LocationSearch getAddresses={getFewAddresses} />)
+  // TODO: fix act warning
+  it('should be able to clear the input with clear button when something is typed', () => {
+    renderComponent({ graphqlMocks: [googleLogo, simpleSuggestions] })
 
     expect(screen.queryByRole('button')).not.toBeInTheDocument()
 
-    const input = screen.getByLabelText(
-      /Address and house number/i
-    ) as HTMLInputElement
+    const input = screen.getByLabelText(LABEL_MATCHER)
     fireEvent.change(input, { target: { value: 'Praia de Botafogo' } })
     const button = screen.getByRole('button')
     expect(button.tabIndex).toBe(UNTABABBLE)
@@ -44,6 +81,15 @@ describe('Location Search', () => {
 
     expect(input).toHaveValue('')
     expect(screen.queryByRole('button')).not.toBeInTheDocument()
+  })
+
+  // TODO: fix act warning
+  it('should be able to clear the input with ESC key when something is typed', () => {
+    renderComponent({ graphqlMocks: [googleLogo, simpleSuggestions] })
+
+    expect(screen.queryByRole('button')).not.toBeInTheDocument()
+
+    const input = screen.getByLabelText(LABEL_MATCHER)
 
     fireEvent.change(input, { target: { value: 'Praia de Botafogo' } })
     fireEvent.keyDown(input, { key: 'Escape', code: 'Escape' })
@@ -52,13 +98,23 @@ describe('Location Search', () => {
     expect(screen.queryByRole('button')).not.toBeInTheDocument()
   })
 
-  it('should display failure message when the search term is not found', () => {
-    render(<LocationSearch getAddresses={() => []} />)
+  it('should display failure message when the search term is not found', async () => {
+    renderComponent({ graphqlMocks: [googleLogo, noSuggestions] })
 
-    const input = screen.getByLabelText(
-      /Address and house number/i
-    ) as HTMLInputElement
+    const input = screen.getByLabelText(LABEL_MATCHER)
+    fireEvent.change(input, { target: { value: 'asdfasdfasdf' } })
+
+    expect(await screen.findByText('No results found')).toBeInTheDocument()
+    fireEvent.blur(input)
+  })
+
+  it('should render provider logo', async () => {
+    renderComponent({ graphqlMocks: [googleLogo, simpleSuggestions] })
+
+    const input = screen.getByLabelText(LABEL_MATCHER)
     fireEvent.change(input, { target: { value: 'Praia de Botafogo' } })
-    expect(screen.getByText('No results found')).toBeInTheDocument()
+
+    expect(await screen.findByAltText('Google logo')).toBeInTheDocument()
+    fireEvent.blur(input)
   })
 })
